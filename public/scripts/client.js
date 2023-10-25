e71964db16b03c380e8730e365f0394773c57f0e
Patrick Aucoin <aucoinp640@gmail.com>
2023-10-25 12:54:55 -0400

    use data-files to get tweets to show up

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,57 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(function() {
 
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  const loadTweets = function() {
+    $.ajax({
+      url: '/tweets',
+      method: "GET",
+      success: function(res) {
+        renderTweets(res);
+      }
+    }
+    )
+  }
+  
+  const renderTweets = function(tweets) {
+    $('#tweets-container').empty()
+    for (let tweet of tweets) {
+     let renderTweet = createTweetElement(tweet) 
+     $('#tweets-container').prepend(renderTweet)
+    }
+  };
+
+  const createTweetElement = function (tweet) {
+    const newTweet = 
+    `
+    <article class="tweet">
+      <div class="tweetHeader">
+        <div style="align-items: center;" >
+          <i "${tweet.user.avatars}"></i>
+          <p>&nbsp&nbsp ${tweet.user.name}</p>
+        </div>  
+        <p>${tweet.user.handle}</p>
+      </div>
+      <form class="tweetText">
+        <p>${escape(tweet.content.text)}</p>
+      </form>
+      <footer class="tweetFooter">
+        <p id="dateText">${timeago.format(tweet.created_at)}</p>
+          <div>
+            <i id="tweetIcon" class="fa-solid fa-flag">&nbsp&nbsp</i>
+            <i id="tweetIcon" class="fa-solid fa-retweet">&nbsp&nbsp</i>
+            <i id="tweetIcon" class="fa-solid fa-heart">&nbsp&nbsp</i>      
+          </div>
+      </footer>
+    </article>
+    `
+      return $(newTweet)
+}
+
+loadTweets();
+}
+)
